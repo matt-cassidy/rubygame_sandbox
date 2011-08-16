8f85ad81567ab08ef83e6eba4193253b0995d864
James MacManus <macmanus.james@gmail.com>
2011-08-16 10:33:51 -0300

    working on vectors and matrices

--- game/views/vectors_view.rb
+++ game/views/vectors_view.rb
@@ -30,24 +30,44 @@ class VectorMan
   attr_reader :pos
   attr_reader :accel
   attr_reader :vel
+  attr_reader :jumping  
   
   def initialize
     @image = Rubygame::Surface.new [20,20]
     @image.fill :green
     @rect = Rubygame::Rect.new [320,240, 20, 20]
     @input = Game::Core::PlayerInput
-    @pos = Vector2.new 320, 240
-    @acc = Vector2.new 0, 1
-    @vel = Vector2.new 0, 0
+    @pos = Game::Core::Vector2.new 320, 240
+    @acc = Game::Core::Vector2.new 0, 1
+    @vel = Game::Core::Vector2.new 0, 0
     @jumping = false
     @moving = false
   end
   
   def on_floor? 
-    @rect.bottom >= 470
+    @rect.bottom <= 470
+  end
+  
+  def jump_key_pressed?
+    @input.key_pressed? :space
   end
   
   def update(seconds)
+    
+    if jump_key_pressed? then
+      vel.y = -10;
+      jumping = true;
+    end
+
+    if jumping then
+      vel.y = vel.y + 1
+      pos.y = pos.y + vel.y
+    end
+
+    if on_floor? then
+      vel.y = 0;
+      jumping = false;
+    end
     
   end
   
